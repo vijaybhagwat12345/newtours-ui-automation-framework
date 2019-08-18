2826a81e6be8e46cfe64ac24be44d13f3874184f
vijaybhagwat12345 <vijay.bhagwat09@gmail.com>
2019-08-18 22:05:57 +0530

    class FirstClass created to existing project from eclipse

---
+++ NewGitProjectDemo/src/FirstClass.java
@@ -0,0 +1,10 @@
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("first class created in FirstClass ");
+	}
+
+}

b654beafc2ace3c75b831ab49b7ad279f595f36d
vijaybhagwat12345 <54233755+vijaybhagwat12345@users.noreply.github.com>
2019-08-18 22:08:34 +0530

    Update FirstClass.java

--- NewGitProjectDemo/src/FirstClass.java
+++ NewGitProjectDemo/src/FirstClass.java
@@ -5,6 +5,7 @@ public class FirstClass {
 		// TODO Auto-generated method stub
 
 		System.out.println("first class created in FirstClass ");
+		System.out.println("let us check whether the changes reflected in eclipse or not");
 	}
 
 }

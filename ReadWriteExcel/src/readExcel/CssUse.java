4ad7ba62cfcdc5846f0440e8125219a893c52a4c
vijaybhagwat12345 <vijay.bhagwat09@gmail.com>
2019-08-19 07:26:53 +0530

    click on datepicker

--- ReadWriteExcel/src/readExcel/CssUse.java
+++ ReadWriteExcel/src/readExcel/CssUse.java
@@ -3,7 +3,9 @@ package readExcel;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class CssUse {
@@ -22,11 +24,22 @@ public class CssUse {
 		driver.manage().window().maximize();
 		driver.get("https://jqueryui.com/datepicker/");
 		Thread.sleep(10000);
+		WebElement framename = driver.findElement(By.className("demo-frame"));
+		
+		driver.switchTo().frame(framename);
 		driver.findElement(By.cssSelector("input#datepicker")).click();
-		driver.findElement(By.cssSelector("input#datepicker")).sendKeys("08/18/2019");
+		Thread.sleep(1000);
+		driver.findElement(By.xpath("//a[@class='ui-datepicker-next ui-corner-all']")).click();
+		Thread.sleep(1000);
+		driver.findElement(By.xpath("//a[@class='ui-datepicker-next ui-corner-all']")).click();
+		driver.findElement(By.xpath("//a[contains(text(),'7')]")).click();
 		
 		
-
+		
+	//	driver.findElement(By.cssSelector("input#datepicker")).sendKeys("08/18/2019");
+		//driver.findElement(By.cssSelector("input#datepicker")).sendKeys(Keys.ENTER);
+		
+		
 		
 	}
 
